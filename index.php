2f571539131ce0b5be2a2d9b7b057da7bd711481
Ziad Omran <omranziad013@gmail.com>
2026-03-15 13:25:12 +0200

    Connect to MySQL using PDO

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 
-require 'router.php';
+$config = require ('config.php');
+
+   $db = new Database($config['database']);
+
+   $id = $_GET['id'];
+   $query = "SELECT * FROM posts WHERE id = :id";
+
+   $posts = $db->query($query,[':id' => $id])->fetch();
+
+
+
 
 
 
